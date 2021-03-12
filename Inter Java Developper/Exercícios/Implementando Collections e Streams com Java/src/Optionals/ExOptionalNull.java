07247e2b8655bf5ae20a5754570ffdb3fa1ced85
João Victor <jgrangeirocosta@gmail.com>
2021-03-12 09:07:35 -0300

    Adiciona Exercicios Optionals

---
+++ Inter Java Developper/Exercícios/Implementando Collections e Streams com Java/src/Optionals/ExOptionalNull.java
@@ -0,0 +1,39 @@
+package Optionals;
+
+import java.util.Optional;
+
+public class ExOptionalNull {
+
+    public static void main(String[] args) {
+
+        // Cria Optional de tipo String
+        Optional<String> meuOptional = Optional.ofNullable(null);
+
+        // Exibe o valor, se presente
+        meuOptional.ifPresent(System.out::println);
+
+        // Lança exceção, se vazio
+        try {
+            meuOptional.orElseThrow(IllegalStateException::new);
+        } catch (IllegalStateException e) {
+            System.out.println("\033[31mjava.lang.IllegalStateException!\033[m");
+        }
+
+        // Exibe mensagem no console, se vazio; transforma o valor, se presente
+        if (meuOptional.isEmpty()) {
+            System.out.println("Optional vazio");
+        } else {
+            meuOptional.map((valor) -> "*" + valor + "* TRANSFORMADO").ifPresent(System.out::println);
+        }
+
+        // Atribui valor a variável, se presente
+        if (meuOptional.isPresent()) {
+            String textoOptional = meuOptional.get();
+            System.out.println("Valor da variável: " + textoOptional);
+        }
+
+        // Filtra o optional com regra de negócio, se presente
+        meuOptional.ifPresent((valor) -> System.out.println("--> " + valor));
+    }
+
+}
